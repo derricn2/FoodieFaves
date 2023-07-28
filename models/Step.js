c22ee8fe9db5e11f3989aca09f0c0619f18e0bad
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-07-27 20:58:41 -0700

    added step model and relationships

---
+++ models/Step.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Step extends Model {}
+
+Step.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        details: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        recipe_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'recipe',
+                key: 'id'
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'step'
+    }
+);
+
+module.exports = Step
