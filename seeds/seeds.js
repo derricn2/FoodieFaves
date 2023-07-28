666521454e140dec07bb490d55cfc8d2d3429a8e
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-07-27 19:35:19 -0700

    added started code to seeds.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,10 +1,29 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Recipe, Step } = require('../models');
 
 const userData = require('./UserData.json');
 const recipeData = require('./RecipeData.json');
 const stepData = require('./StepData.json');
 
 const seedDatabase = async () => {
-    await sequelize.sync({  })
-}
+    await sequelize.sync({ force: false });
+
+    await User.bulkcreate(userData, {
+        individualHooks: true,
+        returning: true
+    });
+
+    await Recipe.bulkcreate(recipeData, {
+        individualHooks: true,
+        returning: true
+    });
+
+    await Step.bulkcreate(stepData, {
+        individualHooks: true,
+        returning: true
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
