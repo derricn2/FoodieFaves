2eb61cf1c4251b9185eca7d63da69fa51b50c7e4
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-08-02 21:22:38 -0700

    aborted favorites routes etc

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const recipeRoutes = require('./RecipeRoutes');
 const userRoutes = require('./UserRoutes');
-const favoriteRoutes = require('./favoritesRoutes');
 
 //maybe change to /recipe and /user
 router.use('/recipes', recipeRoutes);
 router.use('/users', userRoutes);
-router.use('/favorites', favoriteRoutes);
 
 module.exports = router;
